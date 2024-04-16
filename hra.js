7aa0656d0942019399f4d105999766167a99eca3
Jarmila Havířová <161177140+jarmilahavirova@users.noreply.github.com>
2024-04-16 03:46:34 +0200

    Nastavení hledání vítěze dle úkolu 4

--- hra.js
+++ hra.js
@@ -1,7 +1,9 @@
+import { findWinner } from "https://unpkg.com/piskvorky@0.1.4";
+
 let currentPlayer = "circle";
 
-// target posluchač na změnu hráče a s tím souvisejích symbolů
 const selectButton = (event) => {
+  // target posluchač na změnu hráče a s tím souvisejích symbolů
   event.target.disabled = true;
   if (currentPlayer === "circle") {
     event.target.classList.add("game__square--circle");
@@ -12,53 +14,48 @@ const selectButton = (event) => {
     currentPlayer = "circle";
     document.getElementById("current-player").src = "image/circle.svg";
   }
+
+  // vytváření pole(array) symbolů z hracího plánku
+  const playingFieldElms = Array.from(fieldSquares);
+  const playingFieldSymbols = playingFieldElms.map((square) => {
+    console.log(square.classList);
+    if (square.classList.contains("game__square--circle")) {
+      return "o";
+    } else if (square.classList.contains("game__square--cross")) {
+      return "x";
+    } else {
+      return "_";
+    }
+  });
+
+  // hledání vítěze a výpis do alertu
+  const winner = findWinnerRow(playingFieldSymbols);
+  const alertMessage = () => {
+    alert(`Vyhrál hráč se symbolem ${winner}`);
+    location.reload();
+  };
+
+  if (winner === "o" || winner === "x") {
+    setTimeout(alertMessage, 250);
+  } else if (winner === "tie") {
+    setTimeout(() => {
+      alert(`Hra skončila nerozhodně :-o`);
+      location.reload();
+    }, 250);
+  }
 };
 
-// výběr políček bez využití cyklu
-document
-  .querySelector("button:nth-child(1)")
-  .addEventListener("click", selectButton);
+// výběr políček a přiřazení posluchače s použitím cyklů
+const fieldSquares = document.querySelectorAll(".game__square");
 
-document
-  .querySelector("button:nth-child(2)")
-  .addEventListener("click", selectButton);
-
-document
-  .querySelector("button:nth-child(3)")
-  .addEventListener("click", selectButton);
-
-document
-  .querySelector("button:nth-child(4)")
-  .addEventListener("click", selectButton);
-
-document
-  .querySelector("button:nth-child(5)")
-  .addEventListener("click", selectButton);
-
-document
-  .querySelector("button:nth-child(6)")
-  .addEventListener("click", selectButton);
-
-document
-  .querySelector("button:nth-child(7)")
-  .addEventListener("click", selectButton);
-
-document
-  .querySelector("button:nth-child(8)")
-  .addEventListener("click", selectButton);
-
-document
-  .querySelector("button:nth-child(9)")
-  .addEventListener("click", selectButton);
-
-document
-  .querySelector("button:nth-child(10)")
-  .addEventListener("click", selectButton);
+fieldSquares.forEach((square) => {
+  square.addEventListener("click", selectButton);
+});
 
 // Úkol 3 - bonus 1 - ověření, zda chce uživatel opravdu restart hry
 document
   .querySelector(".game__link--restart")
-  .addEventListener("click", function () {
+  .addEventListener("click", function (event) {
     if (!confirm("Opravdu chceš začít znovu?")) {
       event.preventDefault();
     }
